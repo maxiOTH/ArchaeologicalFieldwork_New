a798afc3dcee4920c507e1922e1e3175bdb922cc
unknown <maximilian-bergbauer@web.de>
2019-01-10 11:19:08 +0100

    added LocationHelper

--- app/src/main/java/org/wit/archaeologicalfieldwork/views/editlocation/EditLocationView.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/views/editlocation/EditLocationView.kt
@@ -18,8 +18,7 @@ class EditLocationView : BaseView(), GoogleMap.OnMarkerDragListener, GoogleMap.O
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
 
         presenter = EditLocationPresenter(this)
         mapFragment.getMapAsync{
